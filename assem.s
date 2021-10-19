a8d94804f8d614e1ea6fbad67490017a74ce4a1f
Abdulwadoodd <abdulwadood.afzal88@gmail.com>
2021-10-19 05:03:57 +0500

    Added Assembly support

--- assem.s
+++ assem.s
@@ -1,7 +1,9 @@
-# New instruction tests
 
-# lui x2, 0x12345
-# addi x2,x2, 0x0A8
+
+
+# New instruction tests
+# lui x1, 0x12345
+# addi x1,x1, 0x0A8
 # sb x2, 0(x0)
 # sh x2, 4(x0)
 # sw x2, 8(x0)
@@ -10,13 +12,18 @@
 # lw x5, 8(x0)
 # lbu x6, 8(x0)
 # lhu x7, 8(x0)
-# addi x0,x0,0
-# jal x4, jmp
-# addi x5,x0, 23
-# addi x6,x0, 11
+# jal x2, jmp
+# addi x3,x0, 23
+# addi x4,x0, 11
+# jal x6,end
+
+
 # jmp: 
 # addi x5, x0, 7
-# addi x6, x0, 9
+# jalr x7,x2,0
+
+# end:
+# auipc x5,0x4567
 # addi x0,x0,0
 
 # Logical operation test
@@ -25,16 +32,18 @@
 # slli x3,x1,3
 # srli x3,x1,3
 # srai x4,x2,3
+# addi x0,x0,0
+
 
 # slt with signed and unsigned check    &&      Branch instruction check    
 # addi x1, x0, -3
 # addi x2, x0, 2
-# # sltu x3, x1, x2
-# # slt x4,x1,x2
-# bgeu x2,x1,jmp  #check every branch instruction here
+# sltu x3, x1, x2
+# slt x4,x1,x2
+# bge x2,x1,jmp  #check every branch instruction here
 # addi x4,x0,22
 # jmp:
-# addi x3,x0,69
+# addi x5,x0,69
 # addi x0,x0,0
 
 
